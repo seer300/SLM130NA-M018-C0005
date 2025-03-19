331dd5626019363437f06328fd7952d000168357
seer300 <aasd990@126.com>
2025-03-19 17:10:58 +0800

    添加开机GPIO6 PWM波形输出功能;

--- 1200_SDK/xinyiNBIot_AP/PROJECT/default/src/user_init.c
+++ 1200_SDK/xinyiNBIot_AP/PROJECT/default/src/user_init.c
@@ -1,6 +1,7 @@
 #include "app_init.h"
 #include "at_uart.h"
 #include "hal_gpio.h"
+#include "hal_timer.h"
 
 
 #define GNSS_PRRSTX GPIO_PAD_NUM_20     
@@ -15,6 +16,41 @@ void test(void)
 
 /*section方式定义用户的初始化函数，最终在main入口被依次执行*/
 application_init(test);
+
+/**
+ * @brief 	TIMER 单PWM模式初始化函数
+ * 			这个函数描述了timer初始化为双PWM模式需要的相关步骤.
+ * 			在初始化函数内部需要设置单路PWM的输出引脚与GPIO引脚的对应关系、引脚复用方式
+ * 			以及定时器的编号、工作模式、重载值、时钟分频、PWM比较值、时钟极性等
+ * 			然后打开定时器.
+ */
+void GPIO6_PWM_Init(void)
+{
+	HAL_GPIO_InitTypeDef gpio_init = {0};
+	gpio_init.Pin = GPIO_PAD_NUM_6;
+	gpio_init.Mode = GPIO_MODE_HW_PER;
+	gpio_init.PinRemap = GPIO_TMR2PWM_OUTP;
+	HAL_GPIO_Init(&gpio_init);
+
+	HAL_TIM_HandleTypeDef TimPWMSingleHandle = {0};
+	//用户仅限使用HAL_TIM1和HAL_TIM2
+	TimPWMSingleHandle.Instance = HAL_TIM2;
+	//单输出脉冲宽度（PWM）调制模式
+	TimPWMSingleHandle.Init.Mode = HAL_TIM_MODE_PWM_SINGLE;
+	//设置时钟源分频
+	TimPWMSingleHandle.Init.ClockDivision = HAL_TIM_CLK_DIV_128;
+	//重载值，具体含义请参看本文件的@file相关注释
+	TimPWMSingleHandle.Init.Reload = 15000;
+	//PWM翻转比较值，具体含义请参看本文件的@file相关注释
+	TimPWMSingleHandle.Init.PWMCmp = 5000;
+	//Polarity_Low表示定时器关闭时输出为低电平，开启定时器Count寄存器计数值达到PWM比较值后翻转为高电平
+	TimPWMSingleHandle.Init.Polarity = HAL_TIM_Polarity_Low;
+
+	HAL_TIM_Init(&TimPWMSingleHandle);
+	HAL_TIM_Start(&TimPWMSingleHandle);
+
+    // Send_AT_to_Ext("\r\nGPIO6 PWM init\r\n");
+}
 
 
 /*section方式执行用户定义的初始化函数，即application_init()定义的函数*/
@@ -90,4 +126,8 @@ void User_Startup_Init()
     McuGpioWrite(GNSS_PRRSTX, 1);
     Send_AT_to_Ext("\r\n GNSS On end \r\n");
 #endif
+
+    // GPIO6 输出PWM波形 硬件设计要求
+    GPIO6_PWM_Init();
+
 }
